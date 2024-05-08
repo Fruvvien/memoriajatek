dd61122eee2ed31edbd98e4e6a8d226e8e59a074
Fruvvien <HunFantasztik@gmail.com>
2024-05-08 10:12:07 +0200

    asd

--- memoriajatek/Memory.xaml.cs
+++ memoriajatek/Memory.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,7 +12,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace memoriajatek
 {
@@ -20,12 +24,21 @@ namespace memoriajatek
     public partial class Memory : Window
     {
         public List<Button> buttons = new List<Button>();
+        public List<Button> buttons2 = new List<Button>();
         public List<int> numbers = new List<int>();
+        public int[] ints = new int[2];
+        public List<Button> hiddenButtons = new List<Button>();
+        DispatcherTimer timer;
+        public int counter = 0;
+        public int index = 0;
+        public bool messageBoxShow = false;
         public Memory(int number)
         {
             InitializeComponent();
             GridSetting(number);
-           
+            AddButtons(number);
+            SecondLayerAddButtons(number);
+            timer = new DispatcherTimer();
         }
 
         public void GridSetting(int n)
@@ -42,7 +55,7 @@ namespace memoriajatek
 
                 mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
             }
-            AddButtons(n);
+            
         }
 
         public void AddButtons(int number)
@@ -53,7 +66,7 @@ namespace memoriajatek
                 {
                     Button button = new Button();
                     button.FontSize = 30;
-                    button.Click += ButtonClick;
+                    button.Background = Brushes.Yellow;   
                     Grid.SetRow(button, sor);
                     Grid.SetColumn(button, oszlop);
                     mainGrid.Children.Add(button);
@@ -65,6 +78,26 @@ namespace memoriajatek
             }
             Game(number);
 
+        }
+        public void SecondLayerAddButtons(int number)
+        {
+            for (int sor = 0; sor < number; sor++)
+            {
+                for (int oszlop = 0; oszlop < number; oszlop++)
+                {
+                    Button button = new Button();
+                    button.FontSize = 30;
+                    button.Click += ButtonClick;
+                    button.Content = " ";
+                    Grid.SetRow(button, sor);
+                    Grid.SetColumn(button, oszlop);
+                    mainGrid.Children.Add(button);
+                    buttons2.Add(button);
+
+
+
+                }
+            }
         }
 
         public void Game(int number)
@@ -112,20 +145,106 @@ namespace memoriajatek
         public void ButtonClick(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
+            button.Visibility = Visibility.Hidden;
+            hiddenButtons.Add(button);
+            int row = Grid.GetRow(button);
+            int column = Grid.GetColumn(button);
 
-
-            int[] asd = new int[2];
-            for (int i = 0; i < asd.Length; i++)
+            Button buttonsListContent = buttons.Find(b => Grid.GetRow(b) == row && Grid.GetColumn(b) == column);
+            int szam = int.Parse(buttonsListContent.Content.ToString());
+           
+            
+            ints[index] = szam;
+            if (index < 2)
             {
-                asd[i] = int.Parse(button.Content.ToString());
+                if(hiddenButtons.Count == 2)
+                {
+                    foreach (Button item1 in hiddenButtons)
+                    {
+                        foreach (Button item2 in buttons2)
+                        {
+                            if (item1 != item2)
+                            {
+                                item2.IsEnabled = false;
+                            }
+                        }
+                    }
+                }
+               
+               
+                
+
+                if (ints[0] != 0 && ints[1] != 0)
+                {
+
+                    timer.Interval = TimeSpan.FromSeconds(1);
+                    timer.Tick += (s, args) =>
+                    {
+                        counter++;
+                        if (ints[0] != ints[1])
+                        {
+                            if (counter == 1)
+                            {
+                                timer.Stop();
+                                foreach (Button item in hiddenButtons)
+                                {
+                                    item.Visibility = Visibility.Visible;
+                                }
+                                foreach (Button item2 in buttons2)
+                                {
+
+                                    item2.IsEnabled = true;
+
+                                }
+                                hiddenButtons.Clear();
+                                counter = 0;
+                                
+                            }
+                        }
+                        else
+                        {
+                            timer.Stop();
+                            foreach (Button item1 in hiddenButtons)
+                            {
+                                foreach (Button item2 in buttons2)
+                                {
+                                    if (item1 == item2)
+                                    {
+                                        buttons2.Remove(item1);
+                                        return;
+                                    }
+                                }
+                            }
+                            foreach(Button item2 in buttons2)
+                            {
+                                item2.IsEnabled = true;
+                            }
+
+                            hiddenButtons.Clear();
+                            counter = 0;
+
+
+                        }
+                    };
+                   
+                    timer.Start();
+ 
+                }
+                index++;
             }
-              
+           
+
+            if (index == 2)
+            {
+                index = 0;
+            }
+            //if (win)
+            //{
+                
+            //}
             
 
-               
-            
         }
 
-      
     }
 }
